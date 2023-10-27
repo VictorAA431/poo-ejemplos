dc1535fc3f99f359d60fcff52bb1ee526c4d49d6
Victor <victorarredondo431@gmail.com>
2023-10-26 18:22:11 -0700

    Se renombra clase persona

---
+++ practicas/unidad2/Java/Persona.java
@@ -0,0 +1,17 @@
+class Persona {
+    String nombre;
+    int edad;
+    boolean vive;
+
+    void saludar() {
+        System.out.println("Hola, mi nombre es " + nombre);
+    }
+
+    void incrementarEdad() {
+        edad = edad + 1;
+    }
+
+    void morir() {
+        vive = false;
+    }
+}
