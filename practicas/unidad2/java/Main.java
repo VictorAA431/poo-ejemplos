ca4d199b0552ad07fc15e2884a42ba713b1ce24e
Victor <victorarredondo431@gmail.com>
2023-10-26 19:01:54 -0700

    Problema raiz proyecto

--- practicas/unidad2/java/Main.java
+++ practicas/unidad2/java/Main.java
@@ -1,10 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        Persona victor=new Persona();
-        victor.nombre="Victor";
-        victor.edad=19;
-        victor.vive=true;
-        
+        Persona victor = new Persona();
+        victor.nombre = "Victor";
+        victor.edad = 19;
+        victor.vive = true;
+
         victor.saludar();
     }
 }
